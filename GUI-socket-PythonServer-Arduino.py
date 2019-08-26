dea7c2f9a5a14f62acf592a6252a0af499737f5c
KarimLakra <karimlakra@hotmail.com>
2019-08-26 23:19:58 +0300

    added debug box

--- GUI-socket-PythonServer-Arduino.py
+++ GUI-socket-PythonServer-Arduino.py
@@ -1,16 +1,11 @@
-from PyQt5.QtWidgets import (QMainWindow, QApplication, QLabel, QPushButton, QDialog,
-QGroupBox, QHBoxLayout, QVBoxLayout, QBoxLayout, QDialogButtonBox, QListView, QLineEdit, QScrollArea) # QLayout,
+from PyQt5.QtWidgets import (QMainWindow, QApplication, QLabel, QPushButton,
+    QDialog, QGroupBox, QHBoxLayout, QVBoxLayout, QBoxLayout,  QLayout,
+    QDialogButtonBox, QListView, QLineEdit, QScrollArea)
 from PyQt5 import QtGui, QtWidgets, QtCore
 from PyQt5.QtCore import QRect, Qt, QTimer
 from PyQt5.QtGui import QStandardItemModel, QStandardItem
 
-import sys
-import os
-import signal
-import socket
-import subprocess
-import netifaces
-import getIP_List_func
+import sys, os, signal, socket, subprocess, netifaces, random, getIP_List_func
 
 class Window(QDialog):
     def __init__(self):
@@ -18,7 +13,8 @@ class Window(QDialog):
 
         self.InitWindow()
 
-        # self.IPSel = ''
+        self.RNDbtn = False
+
     def InitWindow(self):
         self.setWindowIcon(QtGui.QIcon("network-port-icon.png"))
         self.setWindowTitle("Socket Server")
@@ -29,17 +25,124 @@ class Window(QDialog):
         self.createLayout3()
 
         vbox = QVBoxLayout()
+        hboxPort_debugLbl = QHBoxLayout()
+
+        hboxPort_debugLbl.addWidget(self.groupBox1)
+        hboxPort_debugLbl.addWidget(self.groupBox2)
+
         vbox.addWidget(self.groupBox)
-        vbox.addWidget(self.groupBox1)
+        vbox.addLayout(hboxPort_debugLbl)
         vbox.addWidget(self.groupBox_servE)
 
         self.setLayout(vbox)
 
         self.show()
 
+    def createLayout1(self):
+        hboxlayout = QHBoxLayout()
+        # hboxlayout.setSizeConstraint(QLayout.SetFixedSize)
+        self.groupBox = QGroupBox("Python Socket server - Arduino")
+
+        hboxlayout.addStretch(1)
+        button = QPushButton("Run server")
+        button.setIcon(QtGui.QIcon("StartServer.png"))
+        button.setIconSize(QtCore.QSize(40,40))
+        button.setToolTip("<h2>Run</h2>the server")    #tooltip with HTML tag
+        button.setMinimumHeight(50)
+        button.setMinimumWidth(120)
+        button.clicked.connect(self.SConnect)
+        hboxlayout.addWidget(button)
+        hboxlayout.addStretch(1)
+
+        button1 = QPushButton("Stop server")
+        button1.setIcon(QtGui.QIcon("StopServer.png"))
+        button1.setIconSize(QtCore.QSize(40,40))
+        button1.setToolTip("<h2>Stop</h2> the server")
+        button1.setMinimumHeight(50)
+        button1.setMinimumWidth(120)
+        button1.clicked.connect(self.SDisconnect)
+        hboxlayout.addWidget(button1)
+        hboxlayout.addStretch(1)
+
+        button2 = QPushButton("Quit")
+        button2.setIcon(QtGui.QIcon("Quit.png"))
+        button2.setIconSize(QtCore.QSize(40,40))
+        button2.setToolTip("<h2>Quit</h2> this application")
+        button2.setMinimumHeight(50)
+        button2.setMinimumWidth(120)
+        button2.clicked.connect(lambda : sys.exit())
+        hboxlayout.addWidget(button2)
+        hboxlayout.addStretch(1)
+
+        self.groupBox.setLayout(hboxlayout)
+
+    def createLayout2(self):
+        hboxLbl_Inp_But = QHBoxLayout()
+        hboxLbl_Inp = QHBoxLayout()
+        vboxLbls = QVBoxLayout()
+        vboxInpts = QVBoxLayout()
+        vboxSearchButton = QVBoxLayout()
+        hboxDebug = QHBoxLayout()
+
+        hboxLbl_Inp_But.setSizeConstraint(QLayout.SetFixedSize)
+
+        self.groupBox1 = QGroupBox("IP && Port:")
+        self.groupBox2 = QGroupBox("Debug:")
+
+        self.label0 = QLabel(self)
+        self.label0.setText("Server IP:")
+        self.label1 = QLabel(self)
+        self.label1.setText("Port:")
+        self.label1.setMaximumHeight(50)
+
+        vboxLbls.addWidget(self.label0)
+        vboxLbls.addWidget(self.label1)
+
+        self.ServerIP = QLineEdit(self)
+        self.ServerIP.setFixedWidth(100)
+        self.ServerIP.setText('192.168.1.2')
+
+        self.PortN = QLineEdit(self)
+        self.PortN.setFixedWidth(100)
+        self.PortN.setText('65432')
+
+        vboxInpts.addWidget(self.ServerIP)
+        vboxInpts.addWidget(self.PortN)
+
+        hboxLbl_Inp.addLayout(vboxLbls)
+        hboxLbl_Inp.addLayout(vboxInpts)
+
+        vboxSearchButton.addStretch(1)
+        buttonSerachAdapter = QPushButton("Search Network Adapters", self)
+        buttonSerachAdapter.setIcon(QtGui.QIcon("search_network.png"))
+        buttonSerachAdapter.setMinimumHeight(50)
+        buttonSerachAdapter.setMinimumWidth(180)
+        buttonSerachAdapter.clicked.connect(self.ChooseIP)
+
+        vboxSearchButton.addWidget(buttonSerachAdapter)
+        vboxSearchButton.addStretch(1)
+
+        hboxLbl_Inp_But.addLayout(hboxLbl_Inp)
+        hboxLbl_Inp_But.addLayout(vboxSearchButton)
+
+        self.labelDebug = QLabel(self)
+        self.labelDebug.setText("Debug")
+
+        self.buttonDebug = QPushButton("RND", self)
+        # self.buttonDebug.setIcon(QtGui.QIcon("search_network.png"))
+        self.buttonDebug.setMinimumHeight(50)
+        self.buttonDebug.setMinimumWidth(180)
+        self.buttonDebug.clicked.connect(self.startRandom)
+        self.buttonDebug.setStyleSheet("background-color: red")
+
+        hboxDebug.addWidget(self.labelDebug)
+        hboxDebug.addWidget(self.buttonDebug)
+
+        self.groupBox1.setLayout(hboxLbl_Inp_But)
+        self.groupBox2.setLayout(hboxDebug)
 
     def createLayout3(self):
-        hboxlayout_serverE = QHBoxLayout()
+        self.hboxlayout_serverE = QHBoxLayout()
         self.groupBox_servE = QGroupBox("Server events")
 
         self.label_servE = QLabel(self)
@@ -49,86 +152,9 @@ class Window(QDialog):
         scroll.setWidget(self.label_servE)
         scroll.setWidgetResizable(True)
         # scroll.setFixedHeight(100)
-        hboxlayout_serverE.addWidget(scroll)
+        self.hboxlayout_serverE.addWidget(scroll)
 
-        self.groupBox_servE.setLayout(hboxlayout_serverE)
-
-    def createLayout1(self):
-        hboxlayout = QHBoxLayout()
-        self.groupBox = QGroupBox("Python Socket server - Arduino")
-
-        button = QPushButton("Run server", self)
-        button.setIcon(QtGui.QIcon("StartServer.png"))
-        button.setIconSize(QtCore.QSize(40,40))
-        button.setToolTip("<h2>Run</h2>the server")    #tooltip with HTML tag
-        button.setMinimumHeight(50)
-        button.clicked.connect(self.SConnect)
-        hboxlayout.addWidget(button)
-
-        button1 = QPushButton("Stop server", self)
-        button1.setIcon(QtGui.QIcon("StopServer.png"))
-        button1.setIconSize(QtCore.QSize(40,40))
-        button1.setToolTip("<h2>Stop</h2> the server")
-        button1.setMinimumHeight(50)
-        button1.clicked.connect(self.SDisconnect)
-        hboxlayout.addWidget(button1)
-
-        button2 = QPushButton("Quit", self)
-        button2.setIcon(QtGui.QIcon("Quit.png"))
-        button2.setIconSize(QtCore.QSize(40,40))
-        button2.setToolTip("<h2>Quit</h2> this application")
-        button2.setMinimumHeight(50)
-        button2.clicked.connect(lambda : sys.exit())
-        hboxlayout.addWidget(button2)
-
-        self.groupBox.setLayout(hboxlayout)
-
-    def createLayout2(self):
-        hboxIP_Port_Search = QHBoxLayout()
-        # hboxIP_Port_Search.setSizeConstraint(QLayout.SetFixedSize)
-        #hboxIP_Port_Search.setFixedWidth(50)
-        vboxFields = QVBoxLayout()
-        vboxSearchButton = QVBoxLayout()
-        hboxIP = QHBoxLayout()
-        hboxPort = QHBoxLayout()
-
-        self.groupBox1 = QGroupBox("IP && Port:")
-
-        self.label0 = QLabel(self)
-        self.label0.setText("Server IP:")
-        self.ServerIP = QLineEdit(self)
-        self.ServerIP.setFixedWidth(200)
-        self.ServerIP.setText('192.168.1.2')
-        hboxIP.addWidget(self.label0)
-        hboxIP.addWidget(self.ServerIP)
-        hboxIP.addStretch(1)
-
-        self.label1 = QLabel(self)
-        self.label1.setText("Port:")
-        self.label1.setMaximumHeight(50)
-        self.PortN = QLineEdit(self)
-        self.PortN.setFixedWidth(100)
-        self.PortN.setText('65432')
-        hboxPort.addWidget(self.label1)
-        hboxPort.addWidget(self.PortN)
-        hboxPort.addStretch(1)
-
-        vboxSearchButton = QVBoxLayout()
-        vboxSearchButton.addStretch(1)
-        buttonSerachAdapter = QPushButton("Search Network Adapters", self)
-        buttonSerachAdapter.setMinimumHeight(50)
-        buttonSerachAdapter.setMinimumWidth(300)
-        buttonSerachAdapter.clicked.connect(self.ChooseIP)
-        vboxSearchButton.addWidget(buttonSerachAdapter)
-        vboxSearchButton.addStretch(1)
-
-        vboxFields.addLayout(hboxIP)
-        vboxFields.addLayout(hboxPort)
-
-        hboxIP_Port_Search.addLayout(vboxFields)
-        hboxIP_Port_Search.addLayout(vboxSearchButton)
-
-        self.groupBox1.setLayout(hboxIP_Port_Search)
+        self.groupBox_servE.setLayout(self.hboxlayout_serverE)
 
     def ChooseIP(self):
         dlg = getIP_List_func.CustomDialog(self)
@@ -156,7 +182,6 @@ class Window(QDialog):
             subprocess.Popen(["python", "-u", "ArduinoSocket.py", ipServ, portN] , stdout = f) # shell = True)
 
 
-
     def SDisconnect(self):
         cmd = 'for /f "tokens=5" %a in (\'netstat -aon ^| find "65432"\') do taskkill /t /f /pid %a'
         os.system(cmd)
@@ -164,11 +189,27 @@ class Window(QDialog):
         self.label_servE.setText("Server Disconnected")
         print("Server Disconnected")
 
-
     def Refresher(self):
         f = open("output.txt", "r")
         self.label_servE.setText(f.read())
         f.close()
+
+    def startRandom(self):
+        if self.RNDbtn == False:
+            self.timerRND = QTimer(self)
+            self.timerRND.timeout.connect(self.randomNum)
+            self.timerRND.start(1000)
+            self.RNDbtn = True
+            self.buttonDebug.setStyleSheet("background-color: green")
+        else:
+            self.timerRND.stop()
+            self.RNDbtn = False
+            self.buttonDebug.setStyleSheet("background-color: red")
+
+    def randomNum(self):
+        rndN = random.randint(1,101)
+        self.labelDebug.setText(str(rndN))
+
 
 if __name__ == "__main__":
     App = QApplication(sys.argv)
