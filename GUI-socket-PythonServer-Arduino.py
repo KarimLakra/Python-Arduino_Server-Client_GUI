88d4c40834156d9954d6952da9848133d6ef5989
KarimLakra <karimlakra@hotmail.com>
2019-08-28 00:08:28 +0300

    replace embeding matplotlib by pyqtgraph

--- GUI-socket-PythonServer-Arduino.py
+++ GUI-socket-PythonServer-Arduino.py
@@ -1,4 +1,4 @@
-import sys, os, signal, socket, subprocess, netifaces, random, getIP_List_func
+import sys, os, signal, socket, subprocess, netifaces, random, getIP_List_func, time
 
 from PyQt5.QtWidgets import (QMainWindow, QApplication, QLabel, QPushButton,
     QDialog, QGroupBox, QHBoxLayout, QVBoxLayout, QBoxLayout, QLayout,
@@ -7,6 +7,7 @@ from PyQt5 import QtGui, QtWidgets, QtCore
 from PyQt5.QtCore import QRect, Qt, QTimer
 from PyQt5.QtGui import QStandardItemModel, QStandardItem
 
+import pyqtgraph as pg
 import numpy as np
 
 import matplotlib
@@ -16,32 +17,117 @@ import matplotlib.pylab as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 
-class MyWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-
-        # uic.loadUi('test.ui', self)
-        self.content_plot = QWidget(self)
-        self.content_plot.resize(800, 350)
-        self.content_plot.move(0,0)
-
-        # test data
-        data = np.array([0.7,0.7,0.7,0.8,0.9,0.9,1.5,1.5,1.5,1.5])
-        fig, ax1 = plt.subplots()
-        bins = np.arange(0.6, 1.62, 0.02)
-        n1, bins1, patches1 = ax1.hist(data, bins, alpha=0.6, density=False, cumulative=False)
-        # plot
-        self.plotWidget = FigureCanvas(fig)
-        # lay = QtWidgets.QVBoxLayout(self.content_plot)
-        lay = QtWidgets.QVBoxLayout(self.content_plot)
-        lay.setContentsMargins(0, 0, 0, 0)
-        lay.addWidget(self.plotWidget)
-        # add toolbar
-        self.addToolBar(QtCore.Qt.BottomToolBarArea, NavigationToolbar(self.plotWidget, self))
-
-# class ButtonsTop(QMainWindow):
+# class MyWindow(QMainWindow):
 #     def __init__(self):
 #         super().__init__()
+#
+#         # uic.loadUi('test.ui', self)
+#         self.content_plot = QWidget(self)
+#         self.content_plot.resize(800, 350)
+#         self.content_plot.move(0,0)
+#
+#         # test data
+#         data = np.array([0.7,0.7,0.7,0.8,0.9,0.9,1.5,1.5,1.5,1.5])
+#         fig, ax1 = plt.subplots()
+#         bins = np.arange(0.6, 1.62, 0.02)
+#         n1, bins1, patches1 = ax1.hist(data, bins, alpha=0.6, density=False, cumulative=False)
+#         # plot
+#         self.plotWidget = FigureCanvas(fig)
+#         # lay = QtWidgets.QVBoxLayout(self.content_plot)
+#         lay = QtWidgets.QVBoxLayout(self.content_plot)
+#         lay.setContentsMargins(0, 0, 0, 0)
+#         lay.addWidget(self.plotWidget)
+#         # add toolbar
+#         self.addToolBar(QtCore.Qt.BottomToolBarArea, NavigationToolbar(self.plotWidget, self))
+
+class WindowPlot(QMainWindow):
+    def __init__(self, parent=None):
+        pg.setConfigOption('background', 'w') #before loading widget
+        super(WindowPlot, self).__init__(parent)
+
+        self.RNDbtn = False
+
+        self.wid = QtGui.QWidget(self)
+        self.setCentralWidget(self.wid)
+
+        vbox = QVBoxLayout()
+        self.createLayout1()
+        vbox.addWidget(self.groupBox)
+        self.wid.setLayout(vbox)
+
+        # self.show()
+
+    def createLayout1(self):
+        vboxlayout = QVBoxLayout()
+        self.groupBox = QGroupBox("GroupBox")
+        self.create_plot()
+        vboxlayout.addLayout(self.layout)
+
+        self.GetDataBtn = QPushButton("Connect")
+        self.GetDataBtn.clicked.connect(self.ticker)
+        self.GetDataBtn.setMinimumHeight(50)
+        self.GetDataBtn.setMinimumWidth(180)
+        vboxlayout.addWidget(self.GetDataBtn)
+
+        self.groupBox.setLayout(vboxlayout)
+
+
+    def create_plot(self):
+
+        self.stream_scroll = pg.PlotWidget(title='Stream Monitor')
+
+        # if not self.parent.daisy_entry.currentIndex():
+        self.channel_count = 16
+        self.buffer_size = 1000
+        samples = 125
+        self.stream_scroll.setYRange(0,1,padding=.01)
+        # else:
+        # self.channel_count = 8
+        #   samples = 250
+        #   self.buffer_size = 2000
+        #   self.stream_scroll.setYRange(-.5,8,padding=.01)
+
+        # self.stream_scroll_time_axis = np.linspace(-5,0,samples)
+        self.stream_scroll.setXRange(0,100, padding=.01)
+        self.stream_scroll.setLabel('bottom','Time','Seconds')
+        self.stream_scroll.setLabel('left','Channel')
+        # for i in range(self.channel_count-1,-1,-1):
+        #   self.data_buffer['buffer_channel{}'.format(i+1)] = deque([0]*self.buffer_size)
+        #   self.filtered_data['filtered_channel{}'.format(i+1)] = deque([0]*samples)
+        #   self.curves['curve_channel{}'.format(i+1)] = self.stream_scroll.plot()
+        #   self.curves['curve_channel{}'.format(i+1)].setData(x=self.stream_scroll_time_axis,y=([point+i+1 for point in self.filtered_data['filtered_channel{}'.format(i+1)]]))
+        self.layout = QtGui.QGridLayout()
+        self.stream_scroll.plotItem.showGrid(True, True, 0.7)
+        self.layout.addWidget(self.stream_scroll,0,0)
+
+    def update1(self):
+        t1=time.process_time()
+        points=100 #number of data points
+        X=np.arange(points)
+        # Y=np.sin(np.arange(points)/points*3*np.pi+time.time())
+        Y=np.random.rand(100)
+        # C=pg.hsvColor(time.time()/5%1,alpha=.5) # Change color each tick
+        C=pg.hsvColor(0,alpha=.5)
+        pen=pg.mkPen(color=C,width=1)
+        self.stream_scroll.plot(X,Y,pen=pen,clear=True)
+        print("update took %.02f ms"%((time.process_time()-t1)*1000))
+        # if self.chkMore.isChecked():
+        #     QtCore.QTimer.singleShot(1, self.update) # QUICKLY repeat
+
+    def ticker(self):
+        if self.RNDbtn == False:
+            self.timer = pg.QtCore.QTimer()
+            self.timer.timeout.connect(self.update1)
+            self.timer.start(1000)
+            print('started')
+            self.RNDbtn = True
+            self.GetDataBtn.setStyleSheet("background-color: green")
+            self.GetDataBtn.setText("Disconnect")
+        else:
+            self.timer.stop()
+            self.RNDbtn = False
+            self.GetDataBtn.setStyleSheet("background-color: red")
+            self.GetDataBtn.setText("Connect")
 
 class Main_Window(QWidget):
     def __init__(self):
@@ -183,7 +269,8 @@ class Main_Window(QWidget):
         hboxPLT = QHBoxLayout()
         self.groupBox_Plt = QGroupBox("Received data")
 
-        plotD = MyWindow()
+        # plotD = MyWindow()
+        plotD = WindowPlot()
 
         hboxlayout_plt.addWidget(plotD)
         # hboxlayout_plt.addStretch()
